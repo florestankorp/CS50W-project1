b986f1108ecefaa324a319c09f037142f1144c7e
Florestan Korp <florestankorp@gmail.com>
2020-04-25 18:17:42 +0200

    import into db works

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
+import json
 import os
+from secrets import key, secret
 
-from flask import Flask, session
+import requests
+from flask import Flask, g, redirect, render_template, request, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -18,9 +21,51 @@ Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+DB = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
 def index():
     return "Project 1: TODO"
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    errors = []
+    if request.method == "POST":
+        if not request.form.get("username"):
+            errors.append("please provide a username")
+
+        elif not request.form.get("password"):
+            errors.append("please provide a password")
+
+        elif request.form.get("password") != request.form.get(
+                "password-confirmation"):
+            errors.append("passwords don't match")
+        else:
+            username = request.form.get("username")
+            password = request.form.get("password")
+            password_confirmation = request.form.get("password-confirmation")
+
+            return redirect("/")
+
+        # store form data
+        # navigate
+
+    return render_template("register.html", errors=errors)
+
+
+# {
+#     'books': [{
+#         'id': 29207858,
+#         'isbn': '1632168146',
+#         'isbn13': '9781632168146',
+#         'ratings_count': 1,
+#         'reviews_count': 6,
+#         'text_reviews_count': 0,
+#         'work_ratings_count': 28,
+#         'work_reviews_count': 129,
+#         'work_text_reviews_count': 9,
+#         'average_rating': '  4.14'
+#     }]
+# }
---
+++ import.py
@@ -0,0 +1,87 @@
+"""
+
+In a Python file called import.py separate from your web application, write a program that will take the books and import them into your PostgreSQL database. You will first need to decide what table(s) to create, what columns those tables should have, and how they should relate to one another. Run this program by running python3 import.py to import the books into your database, and submit this program with the rest of your project code.
+
+"""
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+DB = scoped_session(sessionmaker(bind=engine))
+
+
+def main():
+    DB.execute("""--sql
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255),
+        password VARCHAR(255)
+    )--endsql""")
+
+    # name has to be unique so there are no conflicts when inserting author_id into books table
+    DB.execute("""--sql
+    CREATE TABLE IF NOT EXISTS authors (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE
+    )--endsql""")
+
+    DB.execute("""--sql
+    CREATE TABLE IF NOT EXISTS books (
+        id SERIAL PRIMARY KEY,
+        isbn VARCHAR(255),
+        title VARCHAR(255),
+        year VARCHAR(255),
+        author_id INTEGER REFERENCES authors
+    )--endsql""")
+
+    DB.execute("""--sql
+    CREATE TABLE IF NOT EXISTS reviews (
+        id SERIAL PRIMARY KEY,
+        content TEXT,
+        user_id INTEGER REFERENCES users,
+        book_id INTEGER REFERENCES books
+    )--endsql""")
+
+    print("created tables")
+
+    FILE = open("books.csv")
+    reader = csv.reader(FILE)
+
+    firstline = True
+    for isbn, title, author, year in reader:
+        # skip first line of csv file
+        if firstline:
+            firstline = False
+            continue
+
+        # fill authors table with authors first, so we can use those id's as references when filling the books table
+        DB.execute(
+            """--sql
+        INSERT INTO authors (name) VALUES (:name) ON CONFLICT DO NOTHING
+        --endsql""", {"name": author})
+
+        print("inserted author")
+
+        DB.execute(
+            """--sql
+        INSERT INTO books (isbn, title, year, author_id) VALUES (:isbn, :title, :year, (
+                SELECT id FROM authors WHERE name=:author
+            )
+        ) 
+        --endsql""", {
+                "isbn": isbn,
+                "title": title,
+                "year": year,
+                "author": author,
+            })
+
+        print("inserted book")
+    DB.commit()
+
+
+if __name__ == "__main__":
+    main()
